217d5279c4f3e3eded2c2f625ffb8ec3693cd4b6
Moritz Clasmeier <mtesseract@silverratio.net>
2019-01-24 15:43:56 +0100

    Implement twifferent views

--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -6,17 +6,37 @@ import Reflex.Dom
 import Data.Default
 import qualified Data.Text as Text
 import Common (User)
+import Language.Javascript.JSaddle
+
+data View = View1 | View2 deriving (Eq, Ord, Show)
 
 main :: IO ()
 main = mainWidget $ el "div" $ do
-  buttonRetrieveUsers <- button "Retrieve users from DB"
-  let apiRequestRetrieveUsers = XhrRequest "GET" urlRetrieveUsers def
-      eventRetrieveUsersRequest = const apiRequestRetrieveUsers <$> buttonRetrieveUsers
-  eventRetrieveUsersResponse <- performRequestAsync eventRetrieveUsersRequest
-  let eventUsersRetrieved = (Text.pack . show . decodeUsers) <$> eventRetrieveUsersResponse
-  dynUsersRetrieved <- holdDyn "None" eventUsersRetrieved
-  dynText dynUsersRetrieved
+  buttonViewOne <- fmap (const View1) <$> button "View 1"
+  buttonViewTwo <- fmap (const View2) <$> button "View 2"
+  let changeView = selectView <$> leftmost [buttonViewOne, buttonViewTwo]
+  d <- holdDyn (selectView View2) changeView
+  dyn_ d
+  pure ()
 
   where
-    decodeUsers = decodeXhrResponse :: XhrResponse -> Maybe [User]
-    urlRetrieveUsers = "https://api.silverratio.net/users"
+    selectView :: (DomBuilder t m, MonadJSM (Performable m), HasJSContext (Performable m), PerformEvent t m, TriggerEvent t m, MonadHold t m, PostBuild t m) => View -> m ()
+    selectView = \case
+      View1 -> view1
+      View2 -> view2
+      where
+
+        view1 =
+          text "Hello"
+
+        view2 = do
+          buttonRetrieveUsers <- button "Retrieve users from DB"
+          let apiRequestRetrieveUsers = XhrRequest "GET" urlRetrieveUsers def
+              eventRetrieveUsersRequest = const apiRequestRetrieveUsers <$> buttonRetrieveUsers
+          eventRetrieveUsersResponse <- performRequestAsync eventRetrieveUsersRequest
+          let eventUsersRetrieved = (Text.pack . show . decodeUsers) <$> eventRetrieveUsersResponse
+          dynUsersRetrieved <- holdDyn "None" eventUsersRetrieved
+          dynText dynUsersRetrieved
+          where
+            decodeUsers = decodeXhrResponse :: XhrResponse -> Maybe [User]
+            urlRetrieveUsers = "https://api.silverratio.net/users"
